56e3335325f59ef0d315bc22172f1075bd0ee022
Anh Pham <anh.pham@infodation.vn>
2020-09-08 21:58:29 +0700

    update windows tab

--- cheetsheet/windows_tab/windows_tab.rb
+++ cheetsheet/windows_tab/windows_tab.rb
@@ -5,7 +5,10 @@ driver.navigate.to "https://itmscoaching.herokuapp.com/switch-window"
 
 new_tab_button = driver.find_element(id: "new-tab-button")
 new_tab_button.click
+puts driver.window_handles
 sleep 2
 driver.switch_to.window(driver.window_handles[1])
+# driver.switch_to.window(driver.window_handles.first)
 driver.switch_to.window(driver.window_handles[0])
-sleep 2
+# driver.switch_to.window(driver.window_handles.last)
+
