7ebe9fc49ae1ebac474e8dbae1e0bbd1b90a070b
Anh Pham <anh.pham@infodation.vn>
2020-09-08 20:28:34 +0700

    update browser navigation

--- cheetsheet/browser_navigation/browser_navigation.rb
+++ cheetsheet/browser_navigation/browser_navigation.rb
@@ -9,17 +9,7 @@ driver.get 'http://google.com'
 # driver.navigate.to 'https://selenium.dev'
 ####################################
 
-############ Get current URL #######
-#You can read the current URL from the browser’s address bar using
-driver.current_url
-####################################
-
 ############ Refresh ############
 # Refresh the current page:
 driver.navigate.refresh
-####################################
-
-############ Get title ############
-# You can read the current page title from the browser:
-puts driver.title
 ####################################
