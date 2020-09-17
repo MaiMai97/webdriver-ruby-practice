8e16c9ab5ef5f6455fd02c7690a1a7b441f2bcc7
The iTMS Coaching <61696201+itmscoaching@users.noreply.github.com>
2020-09-17 22:24:37 +0700

    Update get_text.rb

--- cheetsheet/get_element_text/get_text.rb
+++ cheetsheet/get_element_text/get_text.rb
@@ -1,11 +1,10 @@
 require "selenium-webdriver"
 
 driver = Selenium::WebDriver.for :firefox
-driver.navigate.to "https://itmscoaching.herokuapp.com/form"
-driver.find_element(css: '.btn.btn-lg.btn-primary').click
-wait = Selenium::WebDriver::Wait.new(timeout: 10)
-wait.until { driver.find_element(class: 'alert').displayed? }
-banner = driver.find_element(class: 'alert')
-banner_text = banner.text
-puts "BANNER TEXT: #{banner_text}"
+driver.navigate.to "https://itmscoaching.herokuapp.com/buttons"
+button = driver.find_element(xpath: '//button[.="Primary"]')
+button_text = button.text
+button_tagname = button.tag_name
+puts "BUTTON TEXT: #{button_text}"
+puts "BUTTON TAG: #{button_tagname}"
 driver.quit
